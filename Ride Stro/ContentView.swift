c1658c7d7d67fcfdcf5903260b99d35135c36271
Devon Tomlin <devontomlin97@gmail.com>
2024-07-28 11:21:46 -0400

    Initial Commit

---
+++ Ride Stro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ride Stro
+//
+//  Created by devon tomlin on 2024-07-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ride Stro/Ride_StroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ride_StroApp.swift
+//  Ride Stro
+//
+//  Created by devon tomlin on 2024-07-28.
+//
+
+import SwiftUI
+
+@main
+struct Ride_StroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
